3dd23a2177264bca224b23eae816242d0384c6f0
EmptyDreams <minedreams@qq.com>
2021-12-02 12:55:54 +0800

    .

--- code/live2d-widget/autoload.js
+++ code/live2d-widget/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
 //const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
-const live2d_path = "https://cdn.jsdelivr.net/gh/EmptyDreams/resources/code/live2d/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/EmptyDreams/resources/code/live2d-widget/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -32,10 +32,10 @@ if (screen.width >= 768) {
 		loadExternalResource(live2d_path + "waifu-tips.min.js", "js")
 	]).then(() => {
 		initWidget({
-			waifuPath: "/live2d-widget/waifu-tips.json",
+			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
-			cdnPath: "https://cdn.jsdelivr.net/gh/EmptyDreams/resources/code/live2d/api/"
+			cdnPath: "https://cdn.jsdelivr.net/gh/EmptyDreams/resources/code/live2d-api/"
 		});
 	});
 }
